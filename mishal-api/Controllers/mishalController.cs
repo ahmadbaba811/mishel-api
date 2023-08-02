48b4dfc906df791bf57416aa59c132f2be95222d
ahmadbaba811 <ahmadbaba811@hotmail.com>
2023-08-02 18:27:13 +0100

    fixed updates

--- mishal-api/Controllers/mishalController.cs
+++ mishal-api/Controllers/mishalController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Elevate.ElevateDB.Data;
+
 
 namespace mishal_api.Controllers
 {
@@ -19,7 +21,7 @@ namespace mishal_api.Controllers
 
 
         [HttpGet]
-        [Route("api/all_elevated/{code}")]
+        [Route("api/all_elevated-1/{code}")]
         public IHttpActionResult GetElevateDB(string code)
         {
             var connectionString = new EDBConnectionStringBuilder
@@ -28,7 +30,7 @@ namespace mishal_api.Controllers
                 CharSet = "UNICODE",
                 Type = "LOCAL",
                 ConfigPath = "C:\\Program Files\\Atrex\\Shared\\Central",
-                UID = "Administrator",
+                UID = "ADMINISTRATOR",
                 PWD = "EDBDefault"
             };
 
@@ -39,7 +41,8 @@ namespace mishal_api.Controllers
                 DataConnection.Open();
 
                 // Step 5: Execute a Query
-                string sqlQuery = "SELECT * FROM code where StockCode = '" + code + "'";
+                //where StockCode = '" + code + "'
+                string sqlQuery = "SELECT * FROM code";
                 EDBCommand command = new EDBCommand(sqlQuery, DataConnection);
 
                 // Step 6: Retrieve Data
@@ -59,6 +62,87 @@ namespace mishal_api.Controllers
                 // Close the Connection
                 DataConnection.Close();
             }
+        }
+
+
+        [HttpGet]
+        [Route("api/all_elevated/{code}")]
+        public IHttpActionResult GetElevateDB2(string code)
+        {
+            var connectionString = new EDBConnectionStringBuilder
+            {
+                Database = "DirectData",
+                CharSet = "UNICODE",
+                Type = "LOCAL",
+                ConfigPath = "C:\\Program Files\\Atrex\\Shared\\Central",
+                UID = "ADMINISTRATOR",
+                PWD = "EDBDefault",
+            };
+
+            EDBConnection DataConnection = new EDBConnection(connectionString.ToString());
+            string state = DataConnection.State.ToString();
+            try
+            {
+                // Open the connection to the database
+                if (state == "Closed")
+                {
+                    DataConnection.Open();
+
+                    // Step 5: Execute a Query
+                    //where StockCode = '" + code + "'
+                    string sqlQuery = "SELECT * FROM code";
+                    EDBCommand command = new EDBCommand(sqlQuery, DataConnection);
+
+                    // Step 6: Retrieve Data
+                    EDBDataAdapter adapter = new EDBDataAdapter(command);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    if (ds != null)
+                        return Ok(ds.Tables[0]);
+                    return NotFound();
+                }
+                else
+                {
+                    return NotFound();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                DataConnection.Close();
+                return InternalServerError(ex);
+            }
+            finally
+            {
+                // Close the Connection
+                DataConnection.Close();
+            }
+        }
+
+        [HttpGet]
+        [Route("api/elevate")]
+        public IHttpActionResult GetElevated()
+        {
+            string connectionString = "Data Source=192.168.1.76;Initial Catalog=Inventory;User ID=sa;Password=password";
+            EDBConnection connection = new EDBConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                EDBCommand command = new EDBCommand("SELECT * FROM Audit", connection);
+                EDBDataAdapter adapter = new EDBDataAdapter(command);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                if (ds != null)
+                    return Ok(ds.Tables[0]);
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                return InternalServerError(ex);
+            }
+            
         }
     }
 }
